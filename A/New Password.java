ef25dec748b7257b73320a485d8ecb9879946ef2
Suraj Srivastava <118651437+1amSuraj@users.noreply.github.com>
2025-11-08 19:12:06 +0530

    Create New Password.java

---
+++ A/New Password.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class A_New_Password {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            char ch = (char) ('a' + (i % k));
+            System.out.print(ch);
+        }
+    }
+}
